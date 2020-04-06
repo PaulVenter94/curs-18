79c09f48d4a3110698ec2d02112ffb05455d2781
Paul <paulventer94@gmail.com>
2020-04-06 18:52:19 +0300

    Tested null input

--- src/main/java/ro/fasttrackit/curs18/Recursiv.java
+++ src/main/java/ro/fasttrackit/curs18/Recursiv.java
@@ -3,6 +3,7 @@ package ro.fasttrackit.curs18;
 public class Recursiv {
 
     public String lenght(String word) {
+        if (word == null) throw new IllegalArgumentException();
         return word;
     }
 }
